f9ef87a77684af5d0c02af50c18049196160d91d
madlhawa <madhawa242@gmail.com>
2023-04-10 06:33:02 +0530

    added requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+sudo apt update;
+sudo apt -y upgrade;
+
+sudo apt install git;
+sudo apt install -y python3-pip;
+sudo apt install build-essential libssl-dev libffi-dev python3-dev;
+source .profile
+
+pip install scrapy;
+pip install flask;
+git clone https://github.com/Madlhawa/searchit.git;
